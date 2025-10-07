7c480be5c3fae86e9572b8136601cfe63e97addf
Jacobtread <jacobtread@gmail.com>
2025-10-07 18:00:13 +1300

    feat: readme and better options handling

--- README.md
+++ README.md
@@ -1,1 +1,85 @@
 # Vite plugin pdfjs embeddable viewer
+
+This is a vite plugin which makes it easy to embed the [Mozilla pdf.js](https://github.com/mozilla/pdf.js/) PDF viewer into your 
+project.
+
+This plugin exposes options to make it easy to configure for use cases such as:
+- Disabling JavaScript
+- Allow viewing PDF's from another Origin
+
+## Basic Embedding
+
+Below is a basic configuration you probably also want to look at the other options below to
+enable some of the features and options
+
+```ts
+// vite.config.ts
+import pdfjsEmbeddableViewer from "vite-plugin-pdfjs-embeddable-viewer";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [
+    pdfjsEmbeddableViewer(),
+  ],
+});
+```
+
+## Disabling JavaScript
+
+For security reasons you probably don't want PDF's running JavaScript, the following setup disables JavaScript in PDF files: 
+
+
+```ts
+// vite.config.ts
+import pdfjsEmbeddableViewer from "vite-plugin-pdfjs-embeddable-viewer";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [
+    pdfjsEmbeddableViewer({
+      options: {
+        enableScripting: false,
+      },
+    }),
+  ],
+});
+```
+
+## Disable Cross Origin Check
+
+By default the pdf.js viewer prevents cross origin document loading. You can disable
+this default behavior using "allowCrossOrigin"
+
+```ts
+// vite.config.ts
+import pdfjsEmbeddableViewer from "vite-plugin-pdfjs-embeddable-viewer";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [
+    pdfjsEmbeddableViewer({
+      allowCrossOrigin: true, 
+    }),
+  ],
+});
+```
+
+## Include Credentials
+
+To include credentials in the request (Cookies, ...etc) use the following option:
+
+```ts
+// vite.config.ts
+import pdfjsEmbeddableViewer from "vite-plugin-pdfjs-embeddable-viewer";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [
+    pdfjsEmbeddableViewer({
+      openOptions: { 
+        includeCredentials: true 
+      }
+    }),
+  ],
+});
+```
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,127 @@
-export { default as pdfjsEmbeddableViewer } from "./plugin";
+import { normalizePath, type Plugin } from "vite";
+import path from "node:path";
+import { viteStaticCopy, type RenameFunc } from "vite-plugin-static-copy";
+import { fileURLToPath } from "node:url";
+
+interface PdfjsEmbeddableViewerOptions {
+  /**
+   * When enabled the "legacy" version of PDF.js designed to support
+   * older browsers will be used instead
+   *
+   * (Default: false)
+   */
+  legacy?: boolean;
+
+  /**
+   * Options to provide to the viewer, you can find the list
+   * of options in the source code here:
+   *
+   * https://github.com/mozilla/pdf.js/blob/master/web/app_options.js
+   */
+  options?: Record<string, unknown>;
+
+  /**
+   * When enabled the cross origin check that prevent documents from being loaded
+   * from a different origin will be removed.
+   *
+   * (Default: false)
+   */
+  allowCrossOrigin?: boolean;
+
+  /**
+   * Additional configuration options to provide PDFViewerApplication.open() / getDocument
+   * such as the withCredentials: true option that sends credentials with requests
+   */
+  openOptions?: object;
+
+  /**
+   * Custom output directory path name to use when producing the pdfjs
+   * files
+   */
+  customOutputPath?: string;
+}
+
+export default function pdfjsEmbeddableViewer(
+  config: PdfjsEmbeddableViewerOptions
+): Plugin[] {
+  const legacy = config.legacy ?? false;
+  const viewerDirName = legacy ? "pdfjs-legacy" : "pdfjs";
+
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const viewerDirPath = path.resolve(__dirname, "../vendor", viewerDirName);
+  const outputPath = config.customOutputPath ?? "pdfjs";
+
+  const globPath = normalizePath(path.join(viewerDirPath, "**/*"));
+
+  const rename: RenameFunc = (_fileName, _fileExtension, fullPath) => {
+    return normalizePath(path.relative(viewerDirPath, fullPath));
+  };
+
+  return [
+    {
+      name: "vite-plugin-pdfjs-embeddable-viewer",
+    },
+    ...viteStaticCopy({
+      targets: [
+        // Copy all source files
+        {
+          src: globPath,
+          dest: outputPath,
+          rename,
+        },
+        // Patch viewer.js
+        {
+          src: [normalizePath(path.join(viewerDirPath, "web/viewer.mjs"))],
+          dest: outputPath,
+          transform: (content, _fileName) => {
+            const allowCrossOrigin = config.allowCrossOrigin ?? false;
+            if (allowCrossOrigin) {
+              content = content.replace("validateFileURL(file);", "");
+            }
+
+            return content;
+          },
+          rename,
+        },
+        // Patch viewer.html
+        {
+          src: [normalizePath(path.join(viewerDirPath, "web/viewer.html"))],
+          dest: outputPath,
+          transform: (content, _fileName) => {
+            const appOptions = JSON.stringify(config.options ?? {});
+            const openOptions = JSON.stringify(config.openOptions ?? {});
+
+            content = content.replace(
+              "</head>",
+              `
+<script>
+document.addEventListener("webviewerloaded", (event) => {
+    const viewerWindow = event.detail.source;
+    const AppOptions = viewerWindow.PDFViewerApplicationOptions;
+    const PDFViewerApplication = viewerWindow.PDFViewerApplication;
+
+    const appOptions = ${appOptions};
+    AppOptions.setAll(appOptions);
+
+    const originalOpen = PDFViewerApplication.open;
+    const customOptions = ${openOptions};
+
+    PDFViewerApplication.open = function(args) {
+        return originalOpen.call(this, { ...args, ...customOptions })
+    };
+})
+</script>
+</head>
+            `
+            );
+
+            return content;
+          },
+          rename,
+        },
+      ],
+    }),
+  ];
+}
