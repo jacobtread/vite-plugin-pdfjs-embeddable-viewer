3e2e692be368269b4aa3e33e2e6b54e25d53b0ce
jacobtread <jacobtread@gmail.com>
2026-03-20 11:00:50 +1300

    fix: tsdown exports

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
-  target: "node18",
-  dts: true,
-  format: "esm",
+    entry: ["src/index.ts"],
+    target: "node18",
+    dts: true,
+    format: "esm",
+    exports: true,
 });
